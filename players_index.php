0a3d53985395090889d01dc07b82369667585a96
Yaneww11 <93374652+Yaneww11@users.noreply.github.com>
2024-02-20 12:16:30 +0200

    Update players_index.php

--- players_index.php
+++ players_index.php
@@ -25,30 +25,24 @@ if (!isset($_SESSION["user"])){
         <h2 class="heading_players">GOALKEEPERS</h2>
         <hr>
         <div class="row">
-            <div class="column">
-                <div class="card">
-                    <img src="/images/onana.webp" alt="Jane" style="width:100%">
-                    <div class="container">
-                        <h2>Andre Onana</h2>
-                    </div>
+            <div class="card">
+                <img src="/images/onana.webp" alt="Jane" style="width:100%">
+                <div class="container">
+                    <h2>Andre Onana</h2>
                 </div>
             </div>
 
-            <div class="column">
-                <div class="card">
-                    <img src="/images/Atlay.webp" alt="Mike" style="width:100%">
-                    <div class="container">
-                        <h2>Altay Bayindir</h2>
-                    </div>
+            <div class="card">
+                <img src="/images/Atlay.webp" alt="Mike" style="width:100%">
+                <div class="container">
+                    <h2>Altay Bayindir</h2>
                 </div>
             </div>
 
-            <div class="column">
-                <div class="card">
-                    <img src="/images/Heaton.webp" alt="John" style="width:100%">
-                    <div class="container">
-                        <h2>Tom Heaton</h2>
-                    </div>
+            <div class="card">
+                <img src="/images/Heaton.webp" alt="John" style="width:100%">
+                <div class="container">
+                    <h2>Tom Heaton</h2>
                 </div>
             </div>
         </div>
@@ -56,68 +50,52 @@ if (!isset($_SESSION["user"])){
         <h2 class="heading_players">DEFENDERS</h2>
         <hr>
         <div class="row">
-            <div class="column">
-                <div class="card">
-                    <img src="/images/martinez.webp" alt="Martinez" style="width:100%">
-                    <div class="container">
-                        <h2>Lisandro Martinez</h2>
-                    </div>
+            <div class="card">
+                <img src="/images/martinez.webp" alt="Martinez" style="width:100%">
+                <div class="container">
+                    <h2>Lisandro Martinez</h2>
                 </div>
             </div>
 
-            <div class="column">
-                <div class="card">
-                    <img src="/images/varane.webp" alt="Varane" style="width:100%">
-                    <div class="container">
-                        <h2>Raphael Varane</h2>
-                    </div>
+            <div class="card">
+                <img src="/images/varane.webp" alt="Varane" style="width:100%">
+                <div class="container">
+                    <h2>Raphael Varane</h2>
                 </div>
             </div>
 
-            <div class="column">
-                <div class="card">
-                    <img src="/images/maguire.webp" alt="Maguire" style="width:100%">
-                    <div class="container">
-                        <h2>Harry Maguire</h2>
-                    </div>
+            <div class="card">
+                <img src="/images/maguire.webp" alt="Maguire" style="width:100%">
+                <div class="container">
+                    <h2>Harry Maguire</h2>
                 </div>
             </div>
 
-            <div class="column">
-                <div class="card">
-                    <img src="/images/lindelof.webp" alt="Lindelof" style="width:100%">
-                    <div class="container">
-                        <h2>Victor Lindelof</h2>
-                    </div>
+            <div class="card">
+                <img src="/images/lindelof.webp" alt="Lindelof" style="width:100%">
+                <div class="container">
+                    <h2>Victor Lindelof</h2>
                 </div>
             </div>
 
-            <div class="column">
-                <div class="card">
-                    <img src="/images/shawwebp.webp" alt="Shaw" style="width:100%">
-                    <div class="container">
-                        <h2>Luke Shaw </h2>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="column">
-                <div class="card">
-                    <img src="/images/dalot.webp" alt="Dalot" style="width:100%">
-                    <div class="container">
-                        <h2>Diogo Dalot</h2>
-                    </div>
+            <div class="card">
+                <img src="/images/shawwebp.webp" alt="Shaw" style="width:100%">
+                <div class="container">
+                    <h2>Luke Shaw </h2>
                 </div>
             </div>
 
-            <div class="column">
-                <div class="card">
-                    <img src="/images/bissaka.webp" alt="Bissaka" style="width:100%">
-                    <div class="container">
-                        <h2>Aaron Wan-Bissaka</h2>
-                    </div>
+            <div class="card">
+                <img src="/images/dalot.webp" alt="Dalot" style="width:100%">
+                <div class="container">
+                    <h2>Diogo Dalot</h2>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="/images/bissaka.webp" alt="Bissaka" style="width:100%">
+                <div class="container">
+                    <h2>Aaron Wan-Bissaka</h2>
                 </div>
             </div>
         </div>
@@ -125,68 +103,53 @@ if (!isset($_SESSION["user"])){
         <h2 class="heading_players">MIDFIELDERS</h2>
         <hr>
         <div class="row">
-            <div class="column">
-                <div class="card">
-                    <img src="/images/casemiro.webp" alt="Casemiro" style="width:100%">
-                    <div class="container">
-                        <h2>Casemiro</h2>
-                    </div>
+
+            <div class="card">
+                <img src="/images/bruno.webp" alt="Bruno Fernandes" style="width:100%">
+                <div class="container">
+                    <h2>Bruno &#xa9; Fernandes</h2>
                 </div>
             </div>
 
-            <div class="column">
-                <div class="card">
-                    <img src="/images/amrabat.webp" alt="Sofyan Amrabat" style="width:100%">
-                    <div class="container">
-                        <h2>Sofyan Amrabat</h2>
-                    </div>
+            <div class="card">
+                <img src="/images/casemiro.webp" alt="Casemiro" style="width:100%">
+                <div class="container">
+                    <h2>Casemiro</h2>
                 </div>
             </div>
 
-            <div class="column">
-                <div class="card">
-                    <img src="/images/mctominay.webp" alt="Scott McTominay" style="width:100%">
-                    <div class="container">
-                        <h2>Scott McTominay</h2>
-                    </div>
+            <div class="card">
+                <img src="/images/amrabat.webp" alt="Sofyan Amrabat" style="width:100%">
+                <div class="container">
+                    <h2>Sofyan Amrabat</h2>
                 </div>
             </div>
 
-            <div class="column">
-                <div class="card">
-                    <img src="/images/eriksen.webp" alt="Christian Eriksen" style="width:100%">
-                    <div class="container">
-                        <h2>Christian Eriksen</h2>
-                    </div>
+            <div class="card">
+                <img src="/images/mctominay.webp" alt="Scott McTominay" style="width:100%">
+                <div class="container">
+                    <h2>Scott McTominay</h2>
                 </div>
             </div>
 
-            <div class="column">
-                <div class="card">
-                    <img src="/images/bruno.webp" alt="Shaw" style="width:100%">
-                    <div class="container">
-                        <h2>Bruno &#xa9; Fernandes</h2>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="column">
-                <div class="card">
-                    <img src="/images/maimoo.webp" alt="Kobbie Mainoo" style="width:100%">
-                    <div class="container">
-                        <h2>Kobbie Mainoo</h2>
-                    </div>
+            <div class="card">
+                <img src="/images/eriksen.webp" alt="Christian Eriksen" style="width:100%">
+                <div class="container">
+                    <h2>Christian Eriksen</h2>
                 </div>
             </div>
 
-            <div class="column">
-                <div class="card">
-                    <img src="/images/mount.webp" alt="Mason Mount" style="width:100%">
-                    <div class="container">
-                        <h2>Mason Mount</h2>
-                    </div>
+            <div class="card">
+                <img src="/images/maimoo.webp" alt="Kobbie Mainoo" style="width:100%">
+                <div class="container">
+                    <h2>Kobbie Mainoo</h2>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="/images/mount.webp" alt="Mason Mount" style="width:100%">
+                <div class="container">
+                    <h2>Mason Mount</h2>
                 </div>
             </div>
         </div>
@@ -194,52 +157,36 @@ if (!isset($_SESSION["user"])){
         <h2 class="heading_players">ATTACK</h2>
         <hr>
         <div class="row">
-            <div class="column">
-                <div class="card">
-                    <img src="/images/Rashford.webp" alt="Marcus Rashford" style="width:100%">
-                    <div class="container">
-                        <h2>Marcus Rashford</h2>
-                    </div>
+
+            <div class="card">
+                <img src="/images/Rashford.webp" alt="Marcus Rashford" style="width:100%">
+                <div class="container">
+                    <h2>Marcus Rashford</h2>
                 </div>
             </div>
 
-            <div class="column">
-                <div class="card">
-                    <img src="/images/garnacho.webp" alt="Alejandro Garnacho" style="width:100%">
-                    <div class="container">
-                        <h2>Alejandro Garnacho</h2>
-                    </div>
+
+            <div class="card">
+                <img src="/images/garnacho.webp" alt="Alejandro Garnacho" style="width:100%">
+                <div class="container">
+                    <h2>Alejandro Garnacho</h2>
                 </div>
             </div>
 
-            <div class="column">
-                <div class="card">
-                    <img src="/images/mctominay.webp" alt="Scott McTominay" style="width:100%">
-                    <div class="container">
-                        <h2>Scott McTominay</h2>
-                    </div>
+            <div class="card">
+                <img src="/images/antony.webp" alt="Antony" style="width:100%">
+                <div class="container">
+                    <h2>Antony</h2>
                 </div>
             </div>
 
-            <div class="column">
-                <div class="card">
-                    <img src="/images/antony.webp" alt="Antony" style="width:100%">
-                    <div class="container">
-                        <h2>Antony</h2>
-                    </div>
-                </div>
-            </div>
-
-            <div class="column">
-                <div class="card">
-                    <img src="/images/hojlund.webp" alt="Rasmus Hojlund" style="width:100%">
-                    <div class="container">
-                        <h2>Rasmus Hojlund</h2>
-                    </div>
+            <div class="card">
+                <img src="/images/hojlund.webp" alt="Rasmus Hojlund" style="width:100%">
+                <div class="container">
+                    <h2>Rasmus Hojlund</h2>
                 </div>
             </div>
         </div>
     </main>
-
 </body>
 </html>
